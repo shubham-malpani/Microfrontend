0f1e198d6422c3fe3d99315107e4ba0805544f0a
Shubham <shubham.m@pixuate.com>
2024-10-26 23:42:36 +0530

    update app files

--- container/src/App.js
+++ container/src/App.js
@@ -4,7 +4,7 @@ import MarketingApp from "./components/MarketingApp";
 export const App = () => {
   return (
     <div>
-      <h1>Container App</h1>
+      <h1>Container App !!!</h1>
       <hr />
       <h1>Marking App</h1>
       <MarketingApp />
